d68926c28283b7ccd268040fa71cfb76066c04b3
yukkop <hectic.yukkop@gmail.com>
2023-09-16 01:16:31 +0200

    chore: add root `Cargo.toml`

--- crates/client-server/src/feature/ui/mod.rs
+++
@@ -1,6 +0,0 @@
-mod hud;
-pub use hud::HudPlugins;
-
-mod ui;
-pub use ui::UiPlugins;
-pub use ui::FpsPlugins;
--- crates/client-server/src/ui/mod.rs
+++
@@ -1,1 +0,0 @@
-//! Pure reusable ui component modules go here. Things like checkbox or a scoreboard, that is not "effectful" (not tied directly to actual data).
