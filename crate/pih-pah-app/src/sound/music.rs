3dbfb2716c3b125e18603f7b55b8ea14d2582bad
yukkop <hectic.yukkop.it@gmail.com>
2023-11-29 21:17:12 +0100

    fix: menu music timer + music durability

--- crate/pih-pah-app/src/sound/music.rs
+++ crate/pih-pah-app/src/sound/music.rs
@@ -5,35 +5,56 @@ use bevy_kira_audio::prelude::*;
 
 const MINIMAL_DELAY: f32 = 15.;
 const MAXIMAL_DELAY: f32 = 90.;
+const MENU_MUSIC_PATH: &str = "lightslategray_blue.wav";
 
 #[derive(Default, Resource)]
 struct MusicTimer(Timer);
+
+#[derive(Default, Resource)]
+struct MenuMusic(Handle<AudioSource>, Option<Duration>);
 
 pub struct MusicPlugins;
 
 impl Plugin for MusicPlugins {
     fn build(&self, app: &mut App) {
         app
+            .init_resource::<MenuMusic>()
             .init_resource::<MusicTimer>()
             .add_systems(Startup, setup)
             .add_systems(Update, play_music);
     }
 }
 
-fn setup(mut commands: Commands) {
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut menu_music: ResMut<MenuMusic>,
+) {
     commands.insert_resource(MusicTimer(Timer::from_seconds(0.0, TimerMode::Repeating)));
+    let audio_source: Handle<AudioSource> = asset_server.load(MENU_MUSIC_PATH);
+    menu_music.0 = audio_source;
 }
 
-fn play_music(
+fn play_music (
     time: Res<Time>,
-    asset_server: Res<AssetServer>,
     audio: Res<Audio>,
     mut music_timer: ResMut<MusicTimer>,
+    mut menu_music: ResMut<MenuMusic>,
+    audio_sources: Res<Assets<AudioSource>>,
 ) {
     if music_timer.0.tick(time.delta()).just_finished() {
-        audio.play(asset_server.load("lightslategray_blue.wav"));
+        if menu_music.1 == None {
+            if let Some(audio_source) = audio_sources.get(&menu_music.0) {
+                let duration = audio_source.sound.duration();
+                menu_music.1 = Some(duration);
+            } else {
+              return;
+            }
+        }
 
-        let delay = thread_rng().gen_range(MINIMAL_DELAY..MAXIMAL_DELAY);
+        audio.play(menu_music.0.clone());
+
+        let delay = thread_rng().gen_range(MINIMAL_DELAY..MAXIMAL_DELAY) + menu_music.1.unwrap().as_secs_f32();
         music_timer.0.set_duration(Duration::from_secs_f32(delay));
         music_timer.0.reset();
     }
