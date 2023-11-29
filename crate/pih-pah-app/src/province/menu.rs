413ad4fa2d01fab54ad50b20bc41a806e8d9e1db
yukkop <hectic.yukkop.it@gmail.com>
2023-11-29 21:42:59 +0100

    feat: orbit light

--- crate/pih-pah-app/src/province/menu.rs
+++ crate/pih-pah-app/src/province/menu.rs
@@ -1,17 +1,38 @@
 use bevy::prelude::*;
+use std::f32::consts::PI;
 
 const PRIMARY_CAMERA_ORDER: isize = 3;
+
+#[derive(Component)]
+struct OrbitLight {
+    radius: f32,
+    speed: f32,
+    angle: f32,
+}
 
 pub struct MenuPlugins;
 
 impl Plugin for MenuPlugins {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup);
+        app
+            .add_systems(Startup, setup)
+            .add_systems(Update, update_light_position);
     }
 }
 
 fn setup() {
 
+}
+
+fn update_light_position(time: Res<Time>, mut query: Query<(&mut OrbitLight, &mut Transform)>) {
+    for (mut orbit_light, mut transform) in query.iter_mut() {
+        orbit_light.angle += orbit_light.speed * time.delta_seconds();
+        if orbit_light.angle > 2.0 * PI {
+            orbit_light.angle -= 2.0 * PI;
+        }
+        transform.translation.x = orbit_light.radius * orbit_light.angle.cos();
+        transform.translation.z = orbit_light.radius * orbit_light.angle.sin();
+    }
 }
 
 pub fn load<'a>(
@@ -20,7 +41,7 @@ pub fn load<'a>(
     mut materials: ResMut<'a, Assets<StandardMaterial>>,
 ) -> (Commands<'a, 'a>, ResMut<'a, Assets<Mesh>>, ResMut<'a, Assets<StandardMaterial>>) {
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-5., 2.5,  5.).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(5., 2.5,  5.).looking_at(Vec3::ZERO, Vec3::Y),
         camera: Camera {
             order: PRIMARY_CAMERA_ORDER,
             ..default()
@@ -28,15 +49,22 @@ pub fn load<'a>(
         ..Default::default()
     });
 
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 5000.0,
-            shadows_enabled: true,
+    commands.spawn((
+        PointLightBundle {
+            point_light: PointLight {
+                intensity: 5000.0,
+                shadows_enabled: true,
+                ..default()
+            },
+            transform: Transform::from_xyz(0., 8.0, 0.),
             ..default()
         },
-        transform: Transform::from_xyz(0., 4.0, 0.),
-        ..default()
-    });
+        OrbitLight {
+            radius: 8.0,
+            speed: 1.0,
+            angle: 0.0,
+        },
+    ));
 
     commands.spawn((
         PbrBundle {
@@ -46,6 +74,16 @@ pub fn load<'a>(
             ..Default::default()
         },
         Name::new("Terrain"),
+    ));
+
+    commands.spawn((
+        PbrBundle {
+            mesh: mesh.add(Mesh::from(shape::Cube::new(1.0))),
+            material: materials.add(Color::GRAY.into()),
+            transform: Transform::from_xyz(0., 0.5, 0.),
+            ..Default::default()
+        },
+        Name::new("Cube"),
     ));
 
     (commands, mesh, materials)
